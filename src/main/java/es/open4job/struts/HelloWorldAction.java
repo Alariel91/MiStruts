85596f63ca62f901ad4c8a64a92255ce3c6814d7
alariel91 <monysaenzcarrero@gmail.com>
2015-02-05 10:58:13 +0100

    Mediante un formulario en el index.jsp introducir un texto y un nombre, y al darle al boton lo manda al struts y  muestra lo que hay en la pagina hola.jsp que son los mismos datos introducidos.

--- src/main/java/es/open4job/struts/HelloWorldAction.java
+++ src/main/java/es/open4job/struts/HelloWorldAction.java
@@ -2,6 +2,7 @@ package es.open4job.struts;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
@@ -12,6 +13,11 @@ public class HelloWorldAction extends Action{
 	public ActionForward execute(ActionMapping mapping,ActionForm form,
 			HttpServletRequest request,HttpServletResponse response)
 	        throws Exception {
+		String texto= request.getParameter("texto");
+		String nombre= request.getParameter("nombre");
+		HttpSession session=request.getSession(true);
+		session.setAttribute("texto", texto);
+		session.setAttribute("nombre", nombre);
 	
 			return mapping.findForward("success");
 		}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,14 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Formulario</h2>
+<form action="./helloWorld.do" method="get">
+			  <p>Texto:</p> 
+			  <input type="text" name="texto" value="" />
+			  <br/>
+			  <p>Nombre:</p>
+			  <input type="text" name="nombre" value="" />
+			  <br/>
+			  <input type="submit" value="Enviar" />
+	</form>
 </body>
 </html>
